cba9d50836187d983fa0eb3da5c1de83b28b9e57
northpr <north_patawee@hotmail.com>
2023-01-24 14:22:28 +0700

    add detect only one face

--- src/ResNet34/cam_detect.py
+++ src/ResNet34/cam_detect.py
@@ -24,27 +24,32 @@ while True:
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     faces = face_cascade.detectMultiScale(rgb_frame, 1.3, 5)
 
-    # Draw a rectangle around each detected face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        face = rgb_frame[y:y + h, x:x + w]
+    # Check if only one face is detected
+    if len(faces) == 1:
+        # Draw a rectangle around each detected face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            face = rgb_frame[y:y + h, x:x + w]
 
-        # Resize the face to (224, 224)
-        face = cv2.resize(face, (224, 224))
-        face = torch.from_numpy(face).float()
-        face = face.permute(2,0,1)
+            # Manipulate face Resize the face to (224, 224)
+            face = cv2.resize(face, (224, 224))
+            face = torch.from_numpy(face).float()
+            face = face.permute(2,0,1)
 
-        # Normalize the face tensor and predict by using our model
-        face = (face - face.mean()) / face.std()
-        output = model(face.unsqueeze(0))
+            # Normalize the face tensor and predict by using our model
+            face = (face - face.mean()) / face.std()
+            output = model(face.unsqueeze(0))
 
-        # Get the predicted class
-        _, pred = torch.max(output, 1)
-        idx = pred.item()
-        on_screen_display = f"{idx} - {data_dict[idx]['full_name']}"
+            # Get the predicted class
+            _, pred = torch.max(output, 1)
+            idx = pred.item()
+            on_screen_display = f"{idx} - {data_dict[idx]['full_name']}"
 
-        # Draw the predicted class on the frame
-        cv2.putText(frame, on_screen_display, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+            # Draw the predicted class on the frame
+            cv2.putText(frame, on_screen_display, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+    else:
+        cv2.putText(frame, "Please show only one face", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
     # Show the frame
     cv2.imshow('ResNet34', frame)
